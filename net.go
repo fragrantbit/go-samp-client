1c8fd54dec2b8876a01e17f14c639c3e98b345c5
alien <alien>
2021-04-08 04:15:58 +0300

    obvious changes

--- net.go
+++ net.go
@@ -4,23 +4,30 @@ import (
 	"net"
 	"fmt"
 	"strconv"
+	"bundle/bitstream"
 )
 type Peer struct {
 	handle 					net.Conn
 	port   					uint32
 	addr   					string
-	data   					chan []byte
 
-	packetNumber 			int
-	sockfd 					int
-	authDone 				bool
+	recvData   				chan []byte
+	sendData				chan []byte
+	chanBS					chan bitstream.BitStream
 	authKeySent				chan bool
 
-	//dst  				syscall.SockaddrInet4
-	
+	packetNumber 			int
+	authDone 				bool
+
+
+	Client *Client
 }
 
-func InitPeer(addr string, port uint32) *Peer {
+type Client struct {
+	Name string
+} 
+
+func InitPeer(addr string, port uint32, name string) *Peer {
 	conn, err := net.Dial("udp4", addr + ":" + strconv.Itoa(int(port)))
 	if err != nil {
 		fmt.Println("auth: net.Dial failed")
@@ -34,5 +41,9 @@ func InitPeer(addr string, port uint32) *Peer {
 		packetNumber: 0,
 		authDone: false,
 		authKeySent: make(chan bool, 4),
+		chanBS: make(chan bitstream.BitStream),
+		Client: &Client{
+			Name: name,
+		},
 	}
 }
